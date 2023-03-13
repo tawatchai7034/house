6ec842210ce247f7fa532e278e6f4f6c0a891b85
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-13 13:08:28 +0300

    Added ngrx store, effects and dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,9 @@ import { CardComponent } from './features/components/card/card.component';
 import { FooterComponent } from './features/components/footer/footer.component';
 import { NewsletterComponent } from './features/components/newsletter/newsletter.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     NewsletterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })],
   providers: [],
   bootstrap: [AppComponent],
 })
