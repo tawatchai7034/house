437f860561536907eca9dc746c2eacf88f312bce
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-13 09:56:58 +0300

    Updated css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
       <app-header></app-header>
       <app-search-bar style="position: relative; top: -150px;"></app-search-bar>
       <app-content></app-content>
-      <app-newsletter style="position: relative; top: +150px;"></app-newsletter>
+      <app-newsletter style="position: relative; top: +240px;"></app-newsletter>
       <app-footer></app-footer>
     </div>
     <!-- Template End -->
