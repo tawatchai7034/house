c3879a6273a4999663f1a68bb737a4e400d705aa
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-14 10:56:56 +0300

    Updated svg's width and height

--- src/app/features/components/navbar/navbar.component.ts
+++ src/app/features/components/navbar/navbar.component.ts
@@ -7,9 +7,8 @@ import {Component} from '@angular/core';
     <nav class="navbar-container">
       <ul class="navbar-item-container">
         <li class="hotels-section">
-
           <button class="hotels-button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-buildings"
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="white" class="bi bi-buildings"
                  viewBox="0 0 16 16">
               <path
                 d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022ZM6 8.694 1 10.36V15h5V8.694ZM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15Z"/>
