3c326a7d4c5a338d3b9a7d7e6e8b4be7f86dbd36
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-14 10:16:07 +0300

    Added hotel-listing page

---
+++ src/app/pages/hotel-listing/hotel-listing.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-listing',
+  template: `
+    <app-navbar></app-navbar>
+
+  `,
+  styleUrls: ['./hotel-listing.component.css']
+})
+export class HotelListingComponent {
+
+}
