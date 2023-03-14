aebe5e46bb1d873bbf9b648154e8d64c42fe4103
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-14 14:26:22 +0300

    Added active classes for navbar

--- src/app/pages/hotel-listing/hotel-listing.component.ts
+++ src/app/pages/hotel-listing/hotel-listing.component.ts
@@ -3,8 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-hotel-listing',
   template: `
-    <app-navbar></app-navbar>
 
+    <app-navbar class="active"></app-navbar>
+    <h1>Hotel Listing page here</h1>
   `,
   styleUrls: ['./hotel-listing.component.css']
 })
