5957ab99d131988c3fb847529e6c7f1e1edea172
kagan <kagancancubukcu@gmail.com>
2024-08-16 17:37:29 +0300

    Fixed explicit type declarations

--- src/app/features/account/components/user-profile/user-profile.component.ts
+++ src/app/features/account/components/user-profile/user-profile.component.ts
@@ -1,80 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subject, takeUntil } from 'rxjs';
 import { AppStateInterface } from 'src/app/core/models/app-state.model';
 import { loggedInUserSelector } from 'src/app/features/auth/store/auth.selectors';
 
 @Component({
   selector: 'app-user-profile',
-  template: `
-    <!-- Template Start -->
-    <div class="user-profile-container">
-      <div class="user-info-container">
-        <div class="user-info">
-          <div class="user-image-and-name">
-            <div class="user-image-container">
-              <span class="username-logo">{{ firstName | logo }}</span>
-              <div class="pen">
-                <img src="/assets/icons/pencil.svg" alt="" />
-              </div>
-            </div>
-          </div>
-          <div class="user-name-and-email">
-            <p class="username">{{ firstName }} {{ lastName }}</p>
-            <p class="user-email">{{ userEmail }}</p>
-          </div>
-        </div>
-      </div>
-      <div class="account-sections">
-        <section class="account-section-title" (click)="activeTab = 'account'">
-          Account
-        </section>
-        <hr />
-        <section class="account-section-title" (click)="activeTab = 'booking'">
-          Bookings
-        </section>
-        <hr />
-        <section class="account-section-title" (click)="activeTab = 'payment'">
-          Payment methods
-        </section>
-        <hr />
-      </div>
-
-      <app-account-info
-        [loggedInUser]="loggedInUser$ | async"
-        *ngIf="activeTab === 'account'"
-      ></app-account-info>
-
-      <app-bookings-info *ngIf="activeTab === 'booking'"></app-bookings-info>
-      <app-payment-info *ngIf="activeTab === 'payment'"></app-payment-info>
-      <div class="logout-button-container">
-        <button (click)="logout()">LOG OUT</button>
-      </div>
-    </div>
-    <!-- Template End -->
-  `,
+  templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
   loggedInUser$ = this.store.select(loggedInUserSelector);
-  activeTab: string = 'account';
-  firstName: string = '';
-  lastName: string = '';
-  userEmail: string = '';
+  activeTab = 'account';
+  firstName = '';
+  lastName = '';
+  userEmail = '';
 
-  constructor(
-    private store: Store<AppStateInterface>,
-    private router: Router
-  ) {}
+  unsubscribe$ = new Subject<void>();
+
+  constructor(private store: Store<AppStateInterface>, private router: Router) {}
 
   ngOnInit(): void {
-    this.loggedInUser$.subscribe((loggedInUser) => {
+    this.loggedInUser$.pipe(takeUntil(this.unsubscribe$)).subscribe((loggedInUser) => {
       if (loggedInUser) {
         this.firstName = loggedInUser[0].user.firstName;
         this.lastName = loggedInUser[0].user.lastName;
         this.userEmail = loggedInUser[0].user.email;
       } else {
-        console.log(Error);
+        console.error(Error);
       }
     });
   }
--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -4,164 +4,29 @@ import { Store } from '@ngrx/store';
 import { loadStripe } from '@stripe/stripe-js';
 import { AppStateInterface } from 'src/app/core/models/app-state.model';
 import { loggedInUserSelector } from 'src/app/features/auth/store/auth.selectors';
+import { HotelDataModel } from 'src/app/features/hotel/store/hotel.model';
 import { selectSearchResult } from 'src/app/features/hotel/store/search/search.selector';
 import { environment } from 'src/environments/environments';
 @Component({
   selector: 'app-payment',
-  template: `
-    <!-- Template Start -->
-    <app-navbar class="active"></app-navbar>
-    <div class="payment-container">
-      <div class="payment-info-container">
-        <!-- Room information section -->
-        <div class="room-info">
-          <div class="room-details">
-            <h2 class="room-name">{{ room?.name }}</h2>
-            <p class="room-description">{{ room?.description }}</p>
-          </div>
-          <div class="room-price">
-            <p class="price-per-night">{{ room?.price | currency }}/night</p>
-          </div>
-        </div>
-
-        <!-- Hotel information section -->
-        <div class="hotel-info">
-          <div class="hotel-name">{{ hotel.name }}</div>
-          <div class="hotel-address">
-            <img src="/assets/icons/location.svg" alt="" />
-            <span>
-              {{ hotel.address.streetAddress }},{{ hotel.address.city }},{{
-                hotel.address.country
-              }}</span
-            >
-          </div>
-        </div>
-
-        <!-- Check-in and check-out section -->
-        <div class="check-in-out">
-          <div class="check-in">
-            <p class="check-in-label">Check-in</p>
-            <span class="check-in-date">{{ checkInDate }}</span>
-          </div>
-          <div class="hotel-photo">
-            <img class="photo" src="/assets/icons/hotel.svg" alt="" />
-          </div>
-          <div class="check-out">
-            <p class="check-out-label">Check-out</p>
-            <span class="check-out-date">{{ checkOutDate }}</span>
-          </div>
-        </div>
-
-        <ng-container
-          *ngIf="loggedInUser$ | async as loggedInUser; else notLoggedIn"
-        >
-          <div class="credit-card">
-            <div class="credit-card__logo">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/800px-Visa_Inc._logo.svg.png?20170118154621"
-                alt=""
-              />
-            </div>
-            <div class="credit-card__number">
-              <span> **** **** **** ****</span>
-            </div>
-            <div class="credit-card__info">
-              <div class="credit-card__name">
-                {{ firstName }} {{ lastName }}
-              </div>
-              <div class="credit-card__expiry">04/24</div>
-            </div>
-          </div>
-        </ng-container>
-
-        <ng-template #notLoggedIn>
-          <div class="login-signup-container">
-            <span class="login-signup-text">Login or Sign up</span>
-            <button class="login-button" routerLink="/login">Login</button>
-            <button class="signup-button" routerLink="/sign-up">Sign up</button>
-          </div>
-        </ng-template>
-      </div>
-      <div class="card-container">
-        <div class="card-hotel-card">
-          <div class="card-hotel-image-info">
-            <img
-              [src]="hotel.photos[0]"
-              alt="hotel-image"
-              class="card-hotel-image"
-            />
-            <div class="card-hotel-info">
-              <p class="card-hotel-name">{{ hotel?.name }}</p>
-              <p class="card-hotel-room-name">{{ room?.name }}</p>
-              <section class="card-hotel-rating">
-                <span class="rating-score">{{ hotel.rating }}</span>
-                <span class="rating-text"
-                  ><strong>Very good</strong> 54 reviews</span
-                >
-              </section>
-            </div>
-          </div>
-
-          <div class="price-details">
-            <h4>Price Details</h4>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Base Fare</td>
-                  <td>{{ room.price | currency }}</td>
-                </tr>
-                <tr>
-                  <td>Discount</td>
-                  <td>{{ discount | currency }}</td>
-                </tr>
-                <tr>
-                  <td>Taxes</td>
-                  <td>{{ tax | currency }}</td>
-                </tr>
-                <tr>
-                  <td>Service Fee</td>
-                  <td>{{ serviceFee | currency }}</td>
-                </tr>
-                <tr class="total">
-                  <td>Total</td>
-                  <td>{{ totalPrice | currency }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <ng-container
-            *ngIf="loggedInUser$ | async as loggedInUser; else notLoggedInPay"
-          >
-            <div class="pay-button-container">
-              <button #payButton (click)="onCheckout()">Pay Now</button>
-            </div>
-          </ng-container>
-
-          <ng-template #notLoggedInPay> </ng-template>
-        </div>
-      </div>
-    </div>
-    <app-newsletter style="position: relative; top: +240px;"></app-newsletter>
-    <app-footer></app-footer>
-    <!-- Template End -->
-  `,
+  templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
+
 export class PaymentComponent implements OnInit {
-  room: any;
-  hotel: any;
-  firstName: string = '';
-  lastName: string = '';
-  discount: number = 0;
-  tax: number = 0.18;
-  serviceFee: number = 5;
-  totalPrice: number = 0;
+  room: { name: string; price: number, description: string} | undefined;
+  hotel: HotelDataModel | undefined;
+  firstName = '';
+  lastName = '';
+  discount = 0;
+  tax = 0.18;
+  serviceFee = 5;
+  totalPrice = 0;
   paymentHandler: any = null;
   loggedInUser$ = this.store.select(loggedInUserSelector);
-  checkInDate: string = '';
-  checkOutDate: string = '';
-  roomsGuests: string = '';
+  checkInDate = '';
+  checkOutDate = '';
+  roomsGuests = '';
 
   constructor(
     private store: Store<AppStateInterface>,
@@ -173,10 +38,12 @@ export class PaymentComponent implements OnInit {
     const storedHotelName = localStorage.getItem('hotelName');
     const storedRoomName = localStorage.getItem('roomName');
     const storedRoomPrice = localStorage.getItem('roomPrice');
+    const storedRoomDescription = localStorage.getItem('roomDescription');
     if (storedHotelName && storedRoomName && storedRoomPrice) {
       this.room = {
         name: storedRoomName,
         price: Number(storedRoomPrice),
+        description: storedRoomDescription!,
       };
       this.store.select('hotels').subscribe((state) => {
         this.hotel = state.hotels.find((h) => h.name === storedHotelName);
@@ -213,19 +80,18 @@ export class PaymentComponent implements OnInit {
     const numberOfDays = timeDiff / (1000 * 3600 * 24);
 
     // Calculate the total price
-    const basePrice = this.room.price * numberOfGuests * numberOfDays;
+    const basePrice = this.room?.price || 0 * numberOfGuests * numberOfDays;
     const taxAmount = basePrice * this.tax;
     this.totalPrice = basePrice + taxAmount + this.serviceFee;
   }
 
   onCheckout() {
-    this.http
-      .post('http://localhost:8000/checkout', {
+    this.http.post('http://localhost:8000/checkout', {
         name: `${this.firstName} ${this.lastName}'s Payment`,
         amount: this.totalPrice,
-        roomName: this.room.name,
-        roomPhoto: this.hotel.photos[0],
-        hotelName: this.hotel.name,
+        roomName: this.room?.name,
+        roomPhoto: this.hotel?.photos[0],
+        hotelName: this.hotel?.name,
       })
       .subscribe(async (res: any) => {
         let stripe = await loadStripe(this.stripeKey);
